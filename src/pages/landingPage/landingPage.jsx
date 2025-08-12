2863f364885416f5987fe8cf97351760504695f1
Kirubel Tesfazion <tesfk5434@gmail.com>
2025-08-12 19:41:27 -0400

    added custom fonts

--- src/pages/landingPage/landingPage.jsx
+++ src/pages/landingPage/landingPage.jsx
@@ -1,8 +1,8 @@
+import Header from '../../components/header/header'
 import './landingPage.css'
+
 export default function LandingPage(){
     return (
-        <div className='header-container'>
-            <h1 className='header-title'>Travel Guide</h1>
-        </div>
+        <Header/>
     )
 }
