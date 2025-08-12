673febf3500ff7f702b089cca913d7d09d218aa8
Kirubel Tesfazion <tesfk5434@gmail.com>
2025-08-12 19:16:37 -0400

    created header for landing page

---
+++ src/pages/landingPage/landingPage.jsx
@@ -0,0 +1,8 @@
+import './landingPage.css'
+export default function LandingPage(){
+    return (
+        <div className='header-container'>
+            <h1 className='header-title'>Travel Guide</h1>
+        </div>
+    )
+}
