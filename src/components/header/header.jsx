0b7f65250c277ef9bb299c87bbc4aa397d2e17c7
Kirubel Tesfazion <tesfk5434@gmail.com>
2025-08-12 19:42:00 -0400

    added directory for global components

---
+++ src/components/header/header.jsx
@@ -0,0 +1,9 @@
+import './header.css'
+
+export default function Header(){
+    return (
+        <div className='header-container'>
+            <h1 className='header-title'>Travel Guide</h1>
+        </div>
+    )
+}
