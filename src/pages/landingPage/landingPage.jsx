bc4d7b5769e230e627ec4c81ae8640a6d098fc02
Kirubel Tesfazion <tesfk5434@gmail.com>
2025-08-12 20:10:43 -0400

    added content

--- src/pages/landingPage/landingPage.jsx
+++ src/pages/landingPage/landingPage.jsx
@@ -2,6 +2,13 @@ import './landingPage.css'
 import HeaderComponent from '../../components/header/header'
 export default function LandingPage(){
     return (
-        <HeaderComponent/>
+        <>
+            <HeaderComponent/>
+            <div className='content-container'>
+                <div className='content-title-container'>
+                    <h2 className='content-title' >Book your Vacations Plans with Ease</h2>
+                </div>   
+            </div>
+        </>
     )
 }
